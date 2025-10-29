1c3ef448846db04dd4b316211e9671ac58ca984c
scarroz <scarroz@unbosque.edu.co>
2025-10-28 21:24:03 -0500

    Primer Commit de Quickcourier, creacion de repositorio y paquetes

---
+++ src/main/java/co/edu/unbosque/quickcourier/QuickcourierApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.quickcourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickcourierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickcourierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/unbosque/quickcourier/QuickcourierApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.quickcourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcourierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
